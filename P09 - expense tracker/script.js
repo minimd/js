e610904ad342011d533f82c4adffb4771bbdbe6a
mohammed bahaa <139212704+minimd@users.noreply.github.com>
2024-08-07 19:22:41 +0300

    get shit on. expense on fire

---
+++ P09 - expense tracker/script.js
@@ -0,0 +1,19 @@
+let balanceNumber = document.querySelector(".balanceNumber");
+let income = document.querySelector("h4.income");
+let outcome = document.querySelector("h4.expense");
+let boxesSection = document.querySelector(".boxes");
+let cardSection = document.querySelector(".history");
+let textInput = document.querySelector("input[type='text']");
+let amountInput = document.querySelector("input[type='number']");
+let btn = document.querySelector("#center");
+let cards = document.querySelectorAll(".card");
+let expenseNumber = document.querySelectorAll(".expensePrice");
+
+let positive = [];
+let negative = [];
+let all=[];
+let text = "";
+let number = 0;
+let totalIncome = 0;
+let totalOutcome = 0;
+
