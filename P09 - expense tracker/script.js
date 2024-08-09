f354128f71d522c7c8d1a86c84d30e145ddc9217
mohammed bahaa <139212704+minimd@users.noreply.github.com>
2024-08-09 15:09:24 +0300

    expense done.. nice work

--- P09 - expense tracker/script.js
+++ P09 - expense tracker/script.js
@@ -9,11 +9,64 @@ let btn = document.querySelector("#center");
 let cards = document.querySelectorAll(".card");
 let expenseNumber = document.querySelectorAll(".expensePrice");
 
-let positive = [];
-let negative = [];
-let all=[];
-let text = "";
-let number = 0;
-let totalIncome = 0;
-let totalOutcome = 0;
+let transactions = [
+];
+loadTransactions();
+function updateBalance() {
+  let expenses = transactions
+    .filter((t) => t.type === "expense")
+    .reduce((acc, cardSection) => acc + cardSection.amount, 0);
+  outcome.textContent = expenses + "$";
+  let incomes = transactions
+    .filter((t) => t.type === "income")
+    .reduce((acc, curr) => acc + curr.amount, 0);
+  income.textContent = incomes + "$";
+  let balance = transactions.reduce((acc, curr) => acc + curr.amount, 0);
+  balanceNumber.textContent = balance + "$";
+}
+updateBalance();
+function loadCards() {
+  cardSection.innerHTML = "<h2>history</h2>";
+  transactions.forEach((card) => {
+    cardSection.innerHTML += `
+         <div class="card ${card.type == "income" ? "green" : "red"} ">
+          <p class="expenseName">${card.name}</p>
+          <p class="expensePrice">${card.amount}</p>
+        </div>
+        `;
+  });
+  //update cards click event listener to delete transaction
+  cards = document.querySelectorAll(".card");
+  cards.forEach((card, index) => {
+    card.addEventListener("click", function () {
+      transactions.splice(index, 1);
+      updateBalance();
+      loadCards();
+    });
+  });
+  saveTransactions();
+}
+loadCards();
+//add transaction
+btn.addEventListener("click", function () {
+  if (!textInput.value || !amountInput.value) {
+    alert("Please fill all fields");
+    return;
+  }
+  let newTransaction = {
+    name: textInput.value,
 
+    type: amountInput.value > 0 ? "income" : "expense",
+    amount: Number(amountInput.value),
+  };
+  transactions.push(newTransaction);
+  updateBalance();
+  loadCards();
+  saveTransactions();
+});
+function saveTransactions() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+function loadTransactions() {
+  transactions = JSON.parse(localStorage.getItem("transactions"));
+}
